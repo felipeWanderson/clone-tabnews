fe152bb4e69482d0912c2021e6557e47ca290be7
Felipe Leal <felipewanderson2020@gmail.com>
2025-12-16 00:37:19 -0300

    implementado updatedAt no /status

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,5 +1,10 @@
 test("GET to /api/v1/status should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
+  const responseBody = await response.json();
   expect(response.status).toBe(200);
+
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpadatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpadatedAt);
 });
