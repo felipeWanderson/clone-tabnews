560e0141ff201238a4ac79166da745c3717a6538
Felipe Leal <felipewanderson2020@gmail.com>
2026-01-12 23:33:54 -0300

    fix: impedindo SQL injecions

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,25 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const resultServerVersion = await database.query("SHOW server_version;");
+  const postgresVersion = resultServerVersion.rows[0].server_version;
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = resultMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DATABASE;
+  const resultMaxUsedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const maxUsedConnections = resultMaxUsedConnections.rows[0].count;
   return response.status(200).json({
     updated_at: updatedAt,
+    database: {
+      postgres_version: postgresVersion,
+      max_connections: Number(maxConnections),
+      max_used_connections: maxUsedConnections,
+    },
   });
 }
 
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -3,8 +3,11 @@ test("GET to /api/v1/status should return status 200", async () => {
   const responseBody = await response.json();
   expect(response.status).toBe(200);
 
-  expect(responseBody.updated_at).toBeDefined();
   const parsedUpadatedAt = new Date(responseBody.updated_at).toISOString();
 
   expect(responseBody.updated_at).toEqual(parsedUpadatedAt);
+
+  expect(responseBody.database.postgres_version).toEqual("16.0");
+  expect(responseBody.database.max_connections).toEqual(100);
+  expect(responseBody.database.max_used_connections).toEqual(1);
 });
