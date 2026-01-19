95e1ad9c29fccfd85ee6afabef1122f9a3ad580f
Felipe Leal <felipewanderson2020@gmail.com>
2026-01-18 21:02:05 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,23 @@
 import { Client } from "pg";
+import { ssl } from "pg/lib/defaults";
 
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
+    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   console.log("Credenciais do Postgres:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
+    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
+    enviroment: process.env.NODE_ENV,
   });
 
   try {
